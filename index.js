fe53fb3bcf075d74786b98a1beccad39b0809354
KrishnaSingh9098 <arushsingh8103@gmail.com>
2024-05-11 23:46:23 +0530

    BAckend-Series

---
+++ index.js
@@ -0,0 +1,57 @@
+// console.log('hello')
+// let joke = require('give-me-a-joke')
+// console.log(joke,'hijih')
+
+// joke.getRandomDadJoke((jiiii)=>{
+// console.log(jiiii)
+// })
+
+
+
+///  In Synchronus Manner    ///
+
+
+//let fs = require('fs')
+// fs.writeFileSync('index.txt','HEllo THis is TExt file')
+// fs.renameSync('index.txt','index.html')
+// let data = fs.readFileSync('index.html')
+// console.log(data.toString())
+
+// fs.writeFileSync('index.html', 'For NOde JS backend Environment')
+
+
+
+
+
+/// In SYnchronus Manner ///
+
+// fs.writeFile('kohli.rcb','<h2>VIRAT KOHLI COMES IN OPENING PLATFORM</h2>',((err)=>{
+// if(err) throw err
+// console.log("KOHLI DEFINATELY MAKES HER SANTUARY")
+// }))
+
+// fs.rename('kohli.rcb','kohli.txt',((err)=>{
+// if(err) throw err
+// console.log('File Name Is Updated')
+// }))
+
+// fs.readFile('kohli.txt',(data,err)=>{
+//     if(err) throw err
+//     console.log(data.toString())
+// })
+
+
+
+/// creating our own Web server page   ///
+
+// let http =require('http')
+// //console.log(http,'TUmse nhi HOga')
+// let server = http.createServer((req,res)=>{
+// res.end('Hello GUys')
+// })
+// server.listen(5500,()=>{
+//     console.log('Server is now Running on localHost : 5500')
+// })
+
+
+
