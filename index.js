87b2840544217b196c1b3159e3606263db30b21d
KrishnaSingh9098 <arushsingh8103@gmail.com>
2024-05-17 13:57:57 +0530

    third-class

--- index.js
+++ index.js
@@ -59,26 +59,104 @@
 
 /// Creating and taking responsne from server using express  //
 
+// const express = require('express')
+// let app = express()
+// let port = 4500
+
+// app.get('/',(req,res)=>{
+// res.send('Welcome to our Webpage')
+// })
+// app.get('/chotu',(req,res)=>{
+//     res.send('<h2>Chota patakha Don</h2>')
+//     })
+
+//     app.get('/chIO',(req,res)=>{
+//        res.send('<BUTTON>Chota Don</BUTTON>')
+//        })
+
+//        app.get('*',(req,res)=>{
+//           res.send('GHAR JAO BETA')
+//           })
+
+// app.listen(port,()=>{
+// console.log('Your server is running i port Node. 4500')
+// })
+
+
+
+// -------------------------------- 15-05-2024 ------------------------------------------//
+
+
+// pathparameter routing
+// const express = require('express')
+// let  app = express()
+// let port = 5000
+
+// app.get('/',(req,res)=>{
+// res.send('Hello Guys')
+// })
+// app.get('/:a',(req,res)=>{
+//    let {a} = req.params
+//    res.send(a)
+// })
+
+// app.listen(port,()=>{
+// console.log('server is running in high Speed')
+// })
+
+
+//after doing this goto brower and write after   localhost:5000/dog cat but with the help of '/ '.
+
+// app.get('/:r/:a',(req,res)=>{
+//      let {r,a} = req.params
+// console.log(req.params,"hilo")
+//       res.send(r,a)
+// })
+
+// app.listen(port,()=>{
+//    console.log('server is running in high Speed')
+//    })
+   
+
+
+// query parameters //
+
+
+// const express = require('express')
+// let  app = express()
+// let port = 5000
+
+// app.get('/search',(req,res)=>{
+//    let {firstName,lastName} = req.query
+//     res.send(`${firstName}  ${lastName}`)
+// })
+
+// app.listen(port,()=>{
+// console.log('first')
+// })
+
+
+///  ejs templating librery   ///
+
+// app .set('view engine','ejs')
+// app.get('/',(req,res)=>{
+//    res.render('index')
+// })
+
+// app.listen(port,()=>{
+// console.log('server is running : 5000')
+// })
+
+
 const express = require('express')
-let app = express()
-let port = 4500
-
+let  app = express()
+let port = 5000
+app.set()
 app.get('/',(req,res)=>{
-res.send('Welcome to our Webpage')
+res.render('index')
 })
-app.get('/chotu',(req,res)=>{
-    res.send('<h2>Chota patakha Don</h2>')
-    })
-
-    app.get('/chIO',(req,res)=>{
-       res.send('<BUTTON>Chota Don</BUTTON>')
-       })
-
-       app.get('*',(req,res)=>{
-          res.send('GHAR JAO BETA')
-          })
-
 app.listen(port,()=>{
-console.log('Your server is running i port Node. 4500')
-})
+   console.log('server is running : 5000')
+   })
+   
 
